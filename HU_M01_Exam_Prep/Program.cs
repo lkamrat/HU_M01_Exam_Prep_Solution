3e34011d753445f8864c7de49a4bf933b9c1e581
lkamrat <kamrat9@gmail.com>
2017-03-02 08:22:26 +0200

    Add project files.

---
+++ HU_M01_Exam_Prep/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HU_M01_Exam_Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
